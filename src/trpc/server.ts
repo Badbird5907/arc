7cd4daafcc36f86677f5083fbb98c60f791903b2
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-11-30 12:24:15 -0500

    fix: trpc

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-import { createHydrationHelpers } from "@trpc/react-query/rsc";
+import { createHydrationHelpers } from '@trpc/react-query/rsc';
 import { headers } from "next/headers";
 import { cache } from "react";
 
