29468a6a16585bddeaf6130af5eea31d62dca3cd
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-01-04 19:41:06 -0500

    feat: add all available font weights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,17 @@ export const metadata: Metadata = {
   }
 };
 
-const poppins = Poppins({ subsets: ["latin"], weight: "700" });
+const poppins = Poppins({ subsets: ["latin"], weight: [
+  "100",
+  "200",
+  "300",
+  "400",
+  "500",
+  "600",
+  "700",
+  "800",
+  "900"
+] });
 
 export default async function RootLayout({
   children,
