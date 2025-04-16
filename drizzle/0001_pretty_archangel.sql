c4cf47aacf5299811d757f43644605a032952620
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-04-15 20:58:34 -0400

    fix: sql migration bug

--- drizzle/0001_pretty_archangel.sql
+++ drizzle/0001_pretty_archangel.sql
@@ -1,1 +1,5 @@
-ALTER TABLE "queued_commands" ADD COLUMN "executed" boolean DEFAULT false NOT NULL;
+DO $$ BEGIN
+    ALTER TABLE "queued_commands" ADD COLUMN "executed" boolean DEFAULT false NOT NULL;
+EXCEPTION
+    WHEN duplicate_column THEN null;
+END $$;
